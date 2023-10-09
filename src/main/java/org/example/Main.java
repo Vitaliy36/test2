21a5ce96a50652bf7ea250dddb1a834da8c33881
Vitaliy <vitaliykulbaba@gmail.com>
2023-10-09 11:45:40 -0600

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Vital!");
+        System.out.println("Hello Anna!");
+        System.out.println("Hello Avi!");
+        System.out.println("Hello All!");
     }
 }
